df3db06ebf00939655b0983ce31403b068592cff
Jianwu Chen <jianwche@jianwche-mn1.linkedin.biz>
2020-08-25 18:19:58 -0700

    rename safe to safeConsume

--- core/src/main/java/com/jsonex/core/util/LangUtil.java
+++ core/src/main/java/com/jsonex/core/util/LangUtil.java
@@ -14,7 +14,7 @@ public class LangUtil {
   public static <T,R1, R2, R3, R4> R4 safe(T obj, Function<T,R1> getter1, Function<R1,R2> getter2, Function<R2,R3> getter3, Function<R3,R4> getter4) { return safe(safe(obj, getter1, getter2, getter3), getter4); }
   public static <T,R1, R2, R3, R4, R5> R5 safe(T obj, Function<T,R1> getter1, Function<R1,R2> getter2, Function<R2,R3> getter3, Function<R3,R4> getter4, Function<R4,R5> getter5) { return safe(safe(obj, getter1, getter2, getter3, getter4), getter5); }
 
-  public static <T> void safe(T obj, Consumer<T> action) { if (obj != null)  action.accept(obj); }
+  public static <T> void safeConsume(T obj, Consumer<T> action) { if (obj != null)  action.accept(obj); }
 
   public static void doIf(boolean condition, Runnable action) { if (condition) action.run(); }
   public static <T> T orElse(T value, T fullBack) { return value == null ? fullBack : value; }
