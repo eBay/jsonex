12ecfd3a1f0942a49c0336857cbdb6bc267eaade
Jianwu Chen <jianwche@jianwche-mn1.linkedin.biz>
2020-09-23 07:54:40 -0700

    release 0.1.7

--- core/src/main/java/com/jsonex/core/type/Func.java
+++ core/src/main/java/com/jsonex/core/type/Func.java
@@ -1,9 +1,9 @@
 package com.jsonex.core.type;
 
-public class Func {
-  @FunctionalInterface public interface _3<P1, P2, P3, R> { public R apply(P1 p1, P2 p2, P3 p3); }
-  @FunctionalInterface public interface _4<P1, P2, P3, P4, R> { public R apply(P1 p1, P2 p2, P3 p3, P4 p4); }
-  @FunctionalInterface public interface _5<P1, P2, P3, P4, P5, R> { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5); }
-  @FunctionalInterface public interface _6<P1, P2, P3, P4, P5, P6, R> { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6);}
-  @FunctionalInterface public interface _7<P1, P2, P3, P4, P5, P6, P7, R> { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7); }
+public interface Func {
+  @FunctionalInterface interface _3<P1, P2, P3, R> extends Func { public R apply(P1 p1, P2 p2, P3 p3); }
+  @FunctionalInterface interface _4<P1, P2, P3, P4, R> extends Func  { public R apply(P1 p1, P2 p2, P3 p3, P4 p4); }
+  @FunctionalInterface interface _5<P1, P2, P3, P4, P5, R> extends Func  { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5); }
+  @FunctionalInterface interface _6<P1, P2, P3, P4, P5, P6, R> extends Func  { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6);}
+  @FunctionalInterface interface _7<P1, P2, P3, P4, P5, P6, P7, R> extends Func  { public R apply(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5, P6 p6, P7 p7); }
 }
