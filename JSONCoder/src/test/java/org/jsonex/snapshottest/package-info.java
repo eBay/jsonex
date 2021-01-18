9d63e8e5eef0594beb448663cf7b080f716d1074
Jianwu Chen <jianwu@gmail.com>
2021-01-18 12:23:33 -0800

    Use symbol link for snapshottest in jsoncoder test

--- JSONCoder/src/test/java/org/jsonex/snapshottest/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package is copied from sub-module SnapshotTest. Due to circular dependencies, this module can't add dependency to
- * SnapshotTest, so have to copy the whole package here. Please always keep these two packages in sync.
- */
-package org.jsonex.snapshottest;
